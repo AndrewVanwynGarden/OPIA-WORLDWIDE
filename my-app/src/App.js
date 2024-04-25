2a44bcc8baf30cc2e0daae9f58fcf19fa0166863
Andrew <167503082+OpiaUK@users.noreply.github.com>
2024-04-25 04:23:22 +0000

    opia online

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>
+        OPIA: a human-centric operating system that powers the world around you
+        and beyond
+      </h1>
+      <h1>
+        <a href="https://t.me/thisisOPIA">t.me/thisisOPIA</a>
+      </h1>
+      <p>BUY TOKENS AND VOTE ON COMMUNITY GOVERNANCE VIA TELEGRAM</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
